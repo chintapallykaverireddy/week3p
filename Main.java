d06cb0ecb44c6a5a13ced443595139816bd32e87
chintapallykaverireddy <kaverireddy267@gmail.com>
2024-11-26 23:04:44 +0530

    c-1

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+public class Main {
+public static void main(String args[]){
+  int n=15;
+  int c=0;
+  for(int i=2;i<Math.sqrt(n);i++){
+    if(n%i==0){
+      c++;
+      break;
+}}
+if(c==0){
+System.out.println(n+"is prime number");
+}else{
+System.out.println(n+"is not prime number");
+}
+}
+}
